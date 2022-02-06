bc8505befd4a007af66fa2c5ad33c987610ebf3c
cpandares <>
2022-02-06 13:33:52 -0400

    portafolio

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { DiFirebase, DiMysql, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a technologies in the web development world.
+      From Back-end To Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+           * React.js <br />
+           * Angular
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            * Node <br />
+            * Php <br /> 
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiMysql size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Databases</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            * Mysql <br />
+            * MongoDb <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
+);
+
+export default Technologies;
---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Ventmove',
+    description: "Using React, Symfony, Mysql to create a platform to make for all users create a page and integrate your Online Store, Reservation and Collection System, Online Events, Ticket Sales, Contact Manager (CRM), Financing Campaigns, Blog and Much More.",
+      image: '/images/vent.png',
+      tags: ['Symfony', 'Mysql', 'React', 'Sass'],
+    source: '',
+    visit: 'https://ventmove.com/',
+    id: 0,
+  },
+  {
+    title: 'Rv advisor',
+    description:"This start a simple e-commerce for users in USA, with Rv vehicles, providing the opportunity to insure their vehicles, offer road assistance, tire insurance, all under the payment of memberships (using stripe). Today it is a whole social network where users can upload their posts, like systems, chat. Today it continues to grow and add features..",
+    image: '/images/rv.png',
+    tags: ['React', 'Laravel','Mysql', 'Redux'],
+    source: '',
+    visit: 'https://development.thervadvisor.com/Panel',
+    id: 1,
+  },
+  {
+    title: 'Mern Calendar',
+    description: "This is a code repository for the corresponding to an app developed in react & mongoDb with the ability to create users, and add events with a calendar, start date, end date, validations, etc.",
+      image: '/images/calendar.png',
+      tags: ['React', 'WebRTC'],
+    source: 'https://github.com/cpandares/calendar-react',
+    visit: '',
+    id: 2,
+  },
+  {
+    title: 'Journal-app',
+    description: "This is a code repository for the corresponding to one app developed in react, redux & firebase, where users can add notes from different memories, add images. Provide login with google.",
+    image: '/images/journal.png',
+    tags: ['React', 'Firebase'],
+    source: 'https://github.com/cpandares/journal-app',
+    visit: '',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
